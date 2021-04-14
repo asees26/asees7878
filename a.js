3e923d11faaebcf977c448acd094b9e7e575597e
asees26 <56754056+asees26@users.noreply.github.com>
2021-04-14 22:34:16 +0530

    Update a.js

--- a.js
+++ a.js
@@ -1,1 +1,25 @@
-console.log("hello1");
+const puppy = require("puppeteer");
+
+const id ="99667661678";
+
+async function main(){
+
+
+
+    let browser = await puppy.launch({
+        headless: false,
+        defaultViewport: false,
+    });
+
+    let pages = await browser.pages();
+    let tab = pages[0];
+    await tab.goto("https://www.cowin.gov.in/home");
+    await tab.click(".yellowbtn");
+    await tab.type("", id);
+    await tab.click(".next-btn.vac-btn.login-btn.md.button.button-solid.ion-activatable.ion-focusable.hydrated");
+
+
+    
+}
+
+main();
